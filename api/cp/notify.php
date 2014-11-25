2040d27fa0bceb2ad624bc0097942997834d9d7f
Kirill Berezin <enelar@develop-project.ru>
2014-11-25 10:39:05 +0300

    Security rules

--- api/cp/notify.php
+++ api/cp/notify.php
@@ -19,6 +19,9 @@ class notify extends api
 
   private function UpdateWarningProperty( $id, $name, $value )
   {
+    $owner = db::Query("SELECT * FROM track.entity WHERE id=$1 AND uid=$2", [$id, LoadModule('api', 'auth')->uid()], true);
+    if (!$owner())
+      return ["error" => "Недостаточно прав"];
     db::Query("UPDATE track.warnings SET {$name}=$2 WHERE id=$1", [$id, $value]);
   }
 }
